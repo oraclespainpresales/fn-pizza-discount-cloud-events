cd01db70a9dc292a891c0426af570724d7810279
test_oke <ivan.sampedro@oracle.com>
2020-01-21 13:18:58 -0800

    awesome

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM fnproject/fn-java-fdk-build:jdk11-1.0.105 as build-stage
+WORKDIR /function
+ENV MAVEN_OPTS -Dhttp.proxyHost= -Dhttp.proxyPort= -Dhttps.proxyHost= -Dhttps.proxyPort= -Dhttp.nonProxyHosts= -Dmaven.repo.local=/usr/share/maven/ref/repository
+
+ADD pom.xml /function/pom.xml
+RUN ["mvn", "package", "dependency:copy-dependencies", "-DincludeScope=runtime", "-DskipTests=true", "-Dmdep.prependGroupId=true", "-DoutputDirectory=target", "--fail-never"]
+
+ADD src /function/src
+RUN ["mvn", "package", "-DskipTests=true"]
+
+FROM fnproject/fn-java-fdk:jre11-1.0.105
+RUN mkdir .oci
+COPY /oci-config/config /.oci/config
+COPY /oci-config/oci_api_key.pem /.oci/oci_api_key.pem
+
+WORKDIR /function
+COPY --from=build-stage /function/target/*.jar /function/app/
+
+CMD ["com.example.fn.DiscountCampaignUploader::handleRequest"]
--- src/main/java/com/example/fn/DiscountCampaignUploader.java
+++ src/main/java/com/example/fn/DiscountCampaignUploader.java
@@ -1,25 +1,40 @@
 package com.example.fn;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oracle.bmc.auth.AuthenticationDetailsProvider;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.functions.FunctionsInvokeClient;
+import com.oracle.bmc.functions.requests.InvokeFunctionRequest;
+import com.oracle.bmc.functions.responses.InvokeFunctionResponse;
+import com.oracle.bmc.util.StreamUtils;
+
 import io.cloudevents.CloudEvent;
 
-import javax.json.Json;
-import javax.json.stream.JsonParser;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class DiscountCampaignUploader {
 
     public String handleRequest(CloudEvent event) {
+        String responseMess      = "";
+        String invokeEndpointURL = "https://gw7unyffbla.eu-frankfurt-1.functions.oci.oraclecloud.com";
+        String functionId        = "ocid1.fnfunc.oc1.eu-frankfurt-1.aaaaaaaaack6vdtmj7n2wy3caoljvjvbcuexmvvhm3tp2k7673cg4jj3ir4a";
+
         try {
             //get upload file properties like namespace or buckername.
             ObjectMapper objectMapper = new ObjectMapper();
             Map data                  = objectMapper.convertValue(event.getData().get(), Map.class);
             Map additionalDetails     = objectMapper.convertValue(data.get("additionalDetails"), Map.class);
 
-            StringBuilder jsonfileUrl = new StringBuilder("https://objectstorage.us-frankfurt-1.oraclecloud.com/n/")
+            StringBuilder jsonfileUrl = new StringBuilder("https://objectstorage.eu-frankfurt-1.oraclecloud.com/n/")
                     .append(additionalDetails.get("namespace"))
                     .append("/b/")
                     .append(additionalDetails.get("bucketName"))
@@ -27,35 +42,45 @@ public class DiscountCampaignUploader {
                     .append(data.get("resourceName"));
             
             System.out.println("JSON FILE:: " + jsonfileUrl.toString());
-            InputStream is    = new URL(jsonfileUrl.toString()).openStream();
-            JsonParser parser = Json.createParser(is);
-            while (parser.hasNext()) {
-                JsonParser.Event jsonEvent = parser.next();
-                switch(jsonEvent) {
-                   case START_ARRAY:
-                   case END_ARRAY:
-                   case START_OBJECT:
-                   case END_OBJECT:
-                   case VALUE_FALSE:
-                   case VALUE_NULL:
-                   case VALUE_TRUE:
-                      System.out.println(event.toString());
-                      break;
-                   case KEY_NAME:
-                      System.out.print(event.toString() + " " +
-                                       parser.getString() + " - ");
-                      break;
-                   case VALUE_STRING:
-                   case VALUE_NUMBER:
-                      System.out.println(event.toString() + " " +
-                                         parser.getString());
-                      break;
-                }
-             }
+            InputStream isJson = new URL(jsonfileUrl.toString()).openStream();
+
+            JSONTokener tokener = new JSONTokener(isJson);
+			JSONObject joResult = new JSONObject(tokener);
+
+            JSONArray campaigns = joResult.getJSONArray("campaigns");
+            System.out.println("Campaigns:: " + campaigns.length());
+			for (int i = 0; i < campaigns.length(); i++) {
+                JSONObject obj = campaigns.getJSONObject(i);
+                responseMess += invokeCreateCampaingFunction (invokeEndpointURL,functionId,obj.toString());
+            }
         }
         catch (Exception ex){
             ex.printStackTrace();
         }
-        return "OK";
+        return responseMess;
+    }
+
+    private String invokeCreateCampaingFunction (String invokeEndpointURL, String functionId, String payload) throws IOException {
+        String response                            = "";
+        AuthenticationDetailsProvider authProvider = new ConfigFileAuthenticationDetailsProvider("/.oci/config","DEFAULT");
+        
+        //System.out.println("TENANT:: " + authProvider.getTenantId());
+        //System.out.println("USER::   " + authProvider.getUserId());
+        //System.out.println("FINGER:: " + authProvider.getFingerprint());
+        //System.out.println("PATHPK:: " + IOUtils.toString(authProvider.getPrivateKey(), StandardCharsets.UTF_8));
+
+        try (FunctionsInvokeClient fnInvokeClient = new FunctionsInvokeClient(authProvider)){
+            fnInvokeClient.setEndpoint(invokeEndpointURL);
+            InvokeFunctionRequest ifr = InvokeFunctionRequest.builder()
+                    .functionId(functionId)
+                    .invokeFunctionBody(StreamUtils.createByteArrayInputStream(payload.getBytes()))
+                    .build();
+
+            System.err.println("Invoking function endpoint - " + invokeEndpointURL + " with payload " + payload);
+            InvokeFunctionResponse resp = fnInvokeClient.invokeFunction(ifr);
+            response = IOUtils.toString(resp.getInputStream(), StandardCharsets.UTF_8);
+        }
+
+        return response;
     }
 }
